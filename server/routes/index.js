3caec57284519187cfbd13cbde87f111145a5b33
Antonio <ditacj78@gmail.com>
2022-03-19 12:56:25 +0100

    struct : add new auth route & fix requires

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require("./routes/user");
-const Profile = require("./routes/profile");
-const Post = require("./routes/post");
+const User = require('./user');
+const Profile = require('./profile');
+const Post = require('./post');
+const Auth = require('./auth');
 
 // routes
 app.use("/users", User);
 app.use("/profile", Profile);
 app.use("/posts", Post);
+// auth route
+app.use("/auth", Auth);
 
 
 module.exports = router;
