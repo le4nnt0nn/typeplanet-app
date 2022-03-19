30287913d7300f26eff9743314ddc42105da7b23
Antonio <ditacj78@gmail.com>
2022-03-19 12:55:57 +0100

    feat : add auth route and controller for auth

---
+++ server/controllers/auth.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const auth = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { validationResult } = require("express-validator");
+
+/**
+ * @route    GET api/auth
+ * @desc     Testing get for auth
+ * @access   PUBLIC
+ *
+ */
+
+async function getPassword(req, res) {
+    try {
+        // get password from user id
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        // 500 Server Error
+        res.status(500).send('Server error');
+    }
+};
+
+/**
+ * @route    POST api/auth
+ * @desc     Auth user and get token
+ * @access   PUBLIC
+ *
+ */
+
+async function authAndGetToken(req, res) {
+    // store all errors
+    const errors = validationResult(req);
+
+    // check if exists errors
+    // if errors const is not Empty & status code 400 (Bad Request)
+    !errors.isEmpty() && res.status(400).json({ errors: errors.array() });
+
+    // get email & password from body
+    const { email, password } = req.body;
+
+    try {
+        // find user by body email
+        let user = await User.findOne({ email });
+
+        // user not exists with this email
+        if (!user) {
+            return res.status(400).json({ errors: [{ msg: 'Invalid credentials :(' }] });
+        }
+
+        // return jwt with payload
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        // sign with finded user by email
+        jwt.sign(payload, 'mysecrettoken:P', { expiresIn: 360000 },
+            (err, token) => {
+                if (err) throw err;
+                // if not errors, give token
+                res.json({ token });
+            })
+    } catch (err) {
+        // 500 Server Error
+        res.status(500).send('Server error');
+    }
+};
+
+module.exports = {
+    getPassword,
+    authAndGetToken
+};
