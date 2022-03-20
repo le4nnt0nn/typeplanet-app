af603ecce389b473512d9812735e99c92f4c1c24
Antonio <ditacj78@gmail.com>
2022-03-20 19:29:53 +0100

    feat : remove unused import

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const { check } = require("express-validator");
 
 const UserController = require('../controllers/user');
 
