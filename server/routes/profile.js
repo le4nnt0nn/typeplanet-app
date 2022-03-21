b84af108de3cd49e49f6e8e6e7e4b935dc09b242
Antonio <ditacj78@gmail.com>
2022-03-21 20:21:39 +0100

    fix : put existing method for profile POST

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -6,8 +6,8 @@ const ProfileController = require('../controllers/profile');
 const auth = require('../middleware/auth');
 
 router.get('/me', auth, ProfileController.getMyProfile)
-    .post('/', [auth, [check('description', 'Description is required').not().isEmpty(),
-                    check('topics', 'At least ONE topic is required').not().isEmpty()], ProfileController.getAllPosts])
+    .post('/', auth, [check('description', 'Description is required').not().isEmpty(),
+                    check('topics', 'At least ONE topic is required').not().isEmpty()], ProfileController.createProfile)
     .get('/', ProfileController.getAllProfiles)
     .get('/user/:user_id', auth, ProfileController.getProfileByUserId)
     .delete('/', auth, ProfileController.removeProfile)
