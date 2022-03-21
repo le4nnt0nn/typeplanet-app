90404f9540be9345815e5a67659bb2e428e58929
Antonio <ditacj78@gmail.com>
2022-03-21 20:44:28 +0100

    fix : change routes & use bodyParser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
-const routes = require('./routes')
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // general route
 app.use('/api', routes);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
+const app = express();
+const bodyParser = require('body-parser');
 
 const User = require('./user');
 const Profile = require('./profile');
 const Post = require('./post');
 const Auth = require('./auth');
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 // routes
-app.use("/users", User);
-app.use("/profile", Profile);
-app.use("/posts", Post);
+router.use('/users', User);
+router.use('/profile', Profile);
+router.use('/posts', Post);
 // auth route
-app.use("/auth", Auth);
+router.use('/auth', Auth);
 
 
 module.exports = router;
