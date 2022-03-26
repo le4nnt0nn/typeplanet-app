a6e53c75827e3f7ffa9a37e4cbf5d820aa1e2a59
Antonio <ditacj78@gmail.com>
2022-03-27 00:08:57 +0100

    feat : add action types for profile

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,12 @@ export const DELETE_POST = 'DELETE_POST';
 export const ADD_POST = 'ADD_POST';
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+//about profile
+export const GET_PROFILE = 'GET_PROFILE';
+export const GET_PROFILES = 'GET_PROFILES';
+export const PROFILE_ERROR = 'PROFILE_ERROR';
+export const UPDATE_PROFILE = 'UPDATE_PROFILE';
+export const CLEAR_PROFILE = 'CLEAR_PROFILE';
+export const ACCOUNT_DELETE = 'ACCOUNT_DELETE';
+export const GET_REPOS = 'GET_REPOS';
