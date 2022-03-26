c62f52046560682eb411015ec32df0310d403319
Antonio <ditacj78@gmail.com>
2022-03-26 19:06:41 +0100

    feat : add new action for post

---
+++ client/src/actions/post.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import {
+    GET_POSTS,
+    GET_POST,
+    POST_ERROR,
+    PUT_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
+} from './types';
+
+/**
+* @desc Get all posts using /api/posts 
+*/
+
+export const getAllPosts = () => async (dispatch) => {
+    try {
+        // get response from request
+        const res = await axios.get('/api/posts');
+
+        dispatch({
+            type: GET_POSTS,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {
+                msg: err.response.statusText + 'GET_POSTS Error :('
+            }
+        });
+    }
+}
