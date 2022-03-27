4d6b7184835b4978d8c675818c97ce3fd765d24b
Antonio <ditacj78@gmail.com>
2022-03-27 19:51:08 +0200

    feat : implements auth actions

---
+++ client/src/actions/auth.js
@@ -0,0 +1,137 @@
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import {
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    AUTH_ERROR,
+    LOADED_USER,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    DO_LOGOUT,
+    CLEAR_PROFILE
+} from './types';
+
+// TEMPORAL ERROR RESPONSES
+
+/**
+ * @desc loads user with token from localStorage & then takes a response from request
+ */
+
+const loadUser = () => async (dispatch) => {
+    const { token } = localStorage;
+
+    if (token) {
+        // put token to header
+        setAuthToken(token);
+    }
+
+    try {
+        // get res from /api/auth
+        const res = await axios.get('/api/auth');
+        dispatch({
+            type: LOADED_USER,
+            payload: res.data,
+        });
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR,
+            payload: {
+                // text & status from err response
+                msg: err.response.statusText + 'AUTH_ERROR :(',
+                status: err.response.status,
+            }
+        });
+    }
+};
+
+/**
+ * @desc register user getting name, email, password, city & birth 
+ */
+
+const register = ({ name, email, password, city, birth }) => async (dispatch) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const body = JSON.stringify({ name, email, password, city, birth });
+
+    try {
+        // create user with body params and config
+        const res = await axios.post('/api/users', body, config);
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data,
+        });
+
+        // loads user with token
+        dispatch(loadUser());
+
+    } catch (err) {
+        dispatch({
+            type: REGISTER_FAIL,
+            payload: {
+                // text & status from err response
+                msg: err.response.statusText + 'REGISTER_FAIL :(',
+                status: err.response.status,
+            }
+        });
+    }
+};
+
+/**
+ * @desc login user with email & password from body
+ */
+
+const login = ({ email, password }) => async (dispatch) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    const body = JSON.stringify({ email, password });
+
+    try {
+        // login user with body params and config
+        const res = await axios.post('/api/users', body, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data,
+        });
+
+        // loads user with token
+        dispatch(loadUser());
+
+    } catch (err) {
+        dispatch({
+            type: LOGIN_FAIL,
+            payload: {
+                // text & status from err response
+                msg: err.response.statusText + 'LOGIN_FAIL :(',
+                status: err.response.status,
+            }
+        });
+    }
+};
+
+/**
+ * @desc multiple action for logout & clear profile
+ */
+
+const logout = () => async (dispatch) => {
+    dispatch({
+        type: CLEAR_PROFILE
+    });
+    dispatch({
+        type: DO_LOGOUT
+    });
+}
+
+module.exports = {
+    loadUser,
+    register,
+    login,
+    logout
+}
