e56b24c01618b06d43cd7e1d78a4a4aa3ef92c49
Antonio <ditacj78@gmail.com>
2022-03-28 18:57:59 +0200

    feat : init imports for files

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { register } from '../../actions/auth';
