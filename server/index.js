a1a64f70419408c9c1d9d1198c2572beeca462df
Antonio <ditacj78@gmail.com>
2022-03-29 19:58:13 +0200

    fix : add cors for backend

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// middleware
+app.use(cors());
 
 // general route
 app.use('/api', routes);
