1e12984c17c639567059afa5c14615ecf8fb86f2
Antonio <ditacj78@gmail.com>
2022-03-31 20:01:59 +0200

    feat : add Link to Login on button

--- client/src/components/setup/Initial.js
+++ client/src/components/setup/Initial.js
@@ -12,24 +12,24 @@ const Initial = ({ isAuth }) => {
     return (
         <body>
             <section class="gradient-custom">
-                <nav class="float-end d-flex  mr-2">
+                <nav class="float-end d-flex mr-2">
                     <div class="topbar_item"><Link to="/login">Login</Link></div>
                     <div class="topbar_item"><Link to="/register">Create my account</Link></div>
                 </nav>
                 <div class="container py-5 mt-5 text-center">
                     <div class="row">
                         <div class="mt-5 text-center">
-                            <img class="w-25" alt="Logo" src="logo.png" />
+                            <img class="logo" alt="Logo" src="logo.png" />
                             <h3>Hey dev! Join into</h3>
-                            <h1 className="sm:">TypePlanet</h1>
-                            <input type="submit" className="btn btn-outline-light btn-lg px-5 custom-primary-button" value="Discover Now" />
+                            <h1>TypePlanet</h1>
+                            <Link to="/login"><input type="submit" className="btn btn-outline-light btn-lg px-5 custom-primary-button" value="Discover Now" /></Link>
                             <p className="mt-4 text-white">
                                 Already have an account? <Link to="/login" style={{ textDecoration: "none" }}><span class="fw-bold custom-secondary">Welcome Back</span></Link>
                             </p>
                         </div>
                         <div class="mt-5 d-flex-inline text-center">
                             <p class="text-white">TypePlanet © 2022</p>
-                            <img class="w-25 float-right" alt="Astro" src="astro.png" />
+                            <img class="astro float-right" alt="Astro" src="astro.png" />
                         </div>
                     </div>
                 </div>
