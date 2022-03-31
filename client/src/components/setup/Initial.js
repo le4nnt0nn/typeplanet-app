8adb657a1838e8ba1344f65adb9881a91e7ae90c
Antonio <ditacj78@gmail.com>
2022-03-31 18:22:13 +0200

    feat : add initial page

---
+++ client/src/components/setup/Initial.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Initial = ({ isAuth }) => {
+
+    // if user logged then redirects to /home
+    // TODO - REDIRECT HOME
+
+    return (
+        <body>
+            <section class="gradient-custom">
+                <nav class="float-end d-flex  mr-2">
+                    <div class="topbar_item"><Link to="/login">Login</Link></div>
+                    <div class="topbar_item"><Link to="/register">Create my account</Link></div>
+                </nav>
+                <div class="container py-5 mt-5 text-center">
+                    <div class="row">
+                        <div class="mt-5 text-center">
+                            <img class="w-25" alt="Logo" src="logo.png" />
+                            <h3>Hey dev! Join into</h3>
+                            <h1 className="sm:">TypePlanet</h1>
+                            <input type="submit" className="btn btn-outline-light btn-lg px-5 custom-primary-button" value="Discover Now" />
+                            <p className="mt-4 text-white">
+                                Already have an account? <Link to="/login" style={{ textDecoration: "none" }}><span class="fw-bold custom-secondary">Welcome Back</span></Link>
+                            </p>
+                        </div>
+                        <div class="mt-5 d-flex-inline text-center">
+                            <p class="text-white">TypePlanet © 2022</p>
+                            <img class="w-25 float-right" alt="Astro" src="astro.png" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </body>
+    );
+}
+
+Initial.propTypes = {
+    isAuth: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+});
+
+export default connect(mapStateToProps)(Initial);
