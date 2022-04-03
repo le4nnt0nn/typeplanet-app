fb375148b5b7068591fd29c0435b4b89298fe8be
Antonio <ditacj78@gmail.com>
2022-04-03 21:25:51 +0200

    feat : add home component page

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Link, Routes } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getCurrentProfile, removeProfile } from "../../actions/profile";
+
+import './style.css';
+
+// tostify
+import { ToastContainer } from 'react-toastify';
+
+import NavbarRoot from '../setup/Navbar';
+
+/**
+ * @desc fetch data using actions, bring from redux's state and send it to components
+ */
+
+const Home = ({
+    getCurrentProfile,
+    removeProfile,
+    /**auth: { user },
+    profile: { profile } */
+}) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, [getCurrentProfile]);
+
+    return (
+        <>
+            <body>
+                <section class="gradient-custom">
+                    <NavbarRoot />
+                    <div class="container py-5 mt-5 text-center">
+                        <h1>a</h1>
+                    </div>
+                </section>
+            </body>
+        </>
+    )
+};
+
+Home.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    removeProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    profile: state.profile,
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { getCurrentProfile, removeProfile })(Home);
