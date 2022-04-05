af6312e28cdcf72fea1d3663276c33b82e8cdb5a
Antonio <ditacj78@gmail.com>
2022-04-05 20:50:26 +0200

    feat : implement new fileds for weather

--- client/src/components/home/weather/Conditions.js
+++ client/src/components/home/weather/Conditions.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import './style.css';
 
 export const Conditions = (props) => {
     return (
         <div>
             {props.responseObj.cod === 200 ?
                 <div>
-                    <p><strong>{props.responseObj.name}</strong></p>
-                    <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
+                    <h2><strong>{props.responseObj.name}</strong></h2>
+                    <div className='temp-wrapper'>
+                        <h2 className='temp'>{Math.round(props.responseObj.main.temp)}º</h2>
+                    </div>
+                    <img src={`http://openweathermap.org/img/wn/${props.responseObj.weather[0].icon}@2x.png`}></img>
                 </div>
                 : null
             }
         </div>
     )
 }
+
+// description
+// <h2>{props.responseObj.weather[0].description.toUpperCase()}</h2>
