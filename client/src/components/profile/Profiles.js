fa6277db4d019ff4cd936933995cdd3fab67fc0c
Antonio <ditacj78@gmail.com>
2022-04-11 20:25:44 +0200

    feat : add profiles list by id

--- client/src/components/profile/Profiles.js
+++ client/src/components/profile/Profiles.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getAllProfiles } from '../../actions/profile';
+import ProfileCard from './ProfileCard';
 
 const Profiles = ({ getAllProfiles, profile: { profiles } }) => {
     useEffect(() => {
@@ -11,6 +12,15 @@ const Profiles = ({ getAllProfiles, profile: { profiles } }) => {
     return (
         <>
             <h2>profiles</h2>
+            <div>
+                {profiles.length > 0 ? (
+                    profiles.map((profile) => (
+                        <ProfileCard key={profile._id} profile={profile} />
+                    ))
+                ) : (
+                    <h2>Ooops, there is no profile here :( </h2>
+                )}
+            </div>
         </>
     )
 }
