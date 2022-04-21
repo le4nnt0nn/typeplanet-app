fb1207ff542a21c5281e66f0db8dfdba6d21684d
Antonio <ditacj78@gmail.com>
2022-04-21 20:20:05 +0200

    feat : add Posts component

---
+++ client/src/components/post/Posts.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import PostCard from '../post/PostCard';
+import { getAllPosts } from '../../actions/post';
+import NavbarRoot from '../setup/Navbar';
+
+const Posts = ({ getAllPosts, post: { posts } }) => {
+    useEffect(() => {
+        getAllPosts();
+    }, [getAllPosts]);
+
+
+    return (
+        <>
+            <body>
+                <NavbarRoot />
+                <div>
+                    {posts && posts.length > 0 ? (
+                        posts.map((post) => (
+                            <PostCard key={post._id} post={post} />
+                        ))
+                    ) : (
+                        <h2>Ooops, there is no posts here :( </h2>
+                    )}
+                </div>
+            </body>
+        </>
+    )
+}
+
+Posts.propTypes = {
+    getAllPosts: PropTypes.func.isRequired,
+    post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+    post: state.post,
+});
+
+export default connect(mapStateToProps, { getAllPosts })(Posts);
