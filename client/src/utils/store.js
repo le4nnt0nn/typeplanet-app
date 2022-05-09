d98e07d01a3f2a0e36967c0ffa4fcde5523e0ec0
Antonio <ditacj78@gmail.com>
2022-05-09 20:59:06 +0200

    fix : implement redux-persist on store

--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,19 +1,29 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import reducerRoot from '../reducers';
+
+const persistConfig = {
+    key: 'persist-key',
+    storage
+}
 
 const initialState = {};
 
 // middleware with async logic for store
 const middleware = [thunk];
 
+const persistedReducer = persistReducer(persistConfig, reducerRoot)
 const store = createStore (
-    reducerRoot,
+    persistedReducer,
     initialState,
     // apply middleware for store
     composeWithDevTools(applyMiddleware(...middleware))
 );
+const persistor = persistStore(store)
 
 export default store;
+export {persistor};
