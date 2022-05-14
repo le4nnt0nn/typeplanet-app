80273ecc3db29050cef381d88b993ba068855be6
Antonio <ditacj78@gmail.com>
2022-05-14 18:21:18 +0200

    feat : add structure for filter

--- client/src/components/post/Posts.js
+++ client/src/components/post/Posts.js
@@ -8,11 +8,14 @@ import PostMaker from './PostMaker';
 
 const Posts = ({ getAllPosts, post: { posts } }) => {
 
+    // filter
+    let [filter, setFilter] = useState(false);
+
     // unmount component for cleanup
     useEffect(() => {
         let mounted = true
         setTimeout(() => {
-            if(mounted) {
+            if (mounted) {
                 getAllPosts();
             }
         }, 1000)
@@ -24,14 +27,25 @@ const Posts = ({ getAllPosts, post: { posts } }) => {
                 <NavbarRoot />
                 <div>
                     <PostMaker />
+                    <div className="mx-auto text-center m-5">
+                        <button className="btn text-center mx-auto bg-warning" onClick={() => setFilter(true)}>Filter by Interest</button>
+                        <button className="btn text-center mx-auto bg-warning" onClick={() => setFilter(false)}>All</button>
+                    </div>
                 </div>
                 <div>
                     {posts && posts.length > 0 ? (
-                        posts.map((post) => (
-                            <PostCard key={post._id} post={post} />
-                        ))
+                        <div>
+                            {filter === true ? (
+                                // TODO - FILTER HERE
+                                <h2>filtering...</h2>
+                            ) : (
+                                posts.map((post) => (
+                                    <PostCard key={post._id} post={post} />
+                                ))
+                            )}
+                        </div>
                     ) : (
-                        <h2>Ooops, there is no posts here :( </h2>
+                        <h2 className="text-center">Ooops, there is no posts here :( </h2>
                     )}
                 </div>
             </body>
