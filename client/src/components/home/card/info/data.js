f488ea9309939261eb62aff69019c47d04a20192
Antonio <ditacj78@gmail.com>
2022-05-17 20:43:55 +0200

    feat : store random data for cards

---
+++ client/src/components/home/card/info/data.js
@@ -0,0 +1,151 @@
+import general from '../assets/general-info.png';
+import react from '../assets/react-info.png';
+import angular from '../assets/angular-info.png';
+import js from '../assets/javascript-info.png';
+import java from '../assets/java-info.png';
+import c from '../assets/c-info.png';
+
+export const generalInfo = [
+    {
+        id: 1,
+        title: "REST API Best Practices",
+        image: general,
+        url: "https://www.freecodecamp.org/news/rest-api-best-practices-rest-endpoint-design-examples/",
+        txt: "In Web Development, REST APIs play an important role in ensuring smooth communication between the client and the server."
+    },
+    {
+        id: 2,
+        title: "Popular Coding Standars",
+        image: general,
+        url: "https://www.browserstack.com/guide/coding-standards-best-practices",
+        txt: "Think of coding standards as a set of rules, techniques, and best practices to create cleaner, more readable, more efficient code with minimal errors. They offer a uniform format by which software engineers can use to build sophisticated and highly functional code."
+    },
+    {
+        id: 3,
+        title: "Learn Version Control with Git",
+        image: general,
+        url: "https://www.git-tower.com/learn/git/ebook/en/command-line/appendix/best-practices",
+        txt: "Best Practices to improve your skills as developer."
+    },
+];
+
+export const reactInfo = [
+    {
+        id: 4,
+        title: "React Hooks Mega Guide",
+        image: react,
+        url: "https://www.telerik.com/kendo-react-ui/react-hooks-guide/",
+        txt: "First released in October of 2018, the React hook APIs provide an alternative to writing class-based components, and offer an alternative approach to state management and lifecycle methods. Hooks bring to functional components the things we once were only able to do with classes, like being able to work with React local state, effects and context through useState, useEffect and useContext."
+    },
+    {
+        id: 5,
+        title: "Understanding UseEffect",
+        image: react,
+        url: "https://linguinecode.com/post/getting-started-with-react-useeffect",
+        txt: "React useEffect is a function that gets executed for 3 different React component lifecycles."
+    },
+    {
+        id: 6,
+        title: "React Dev Tools",
+        image: react,
+        url: "https://blog.openreplay.com/an-introduction-to-react-dev-tools",
+        txt: "React team has created an official React Dev Tools browser extension (Chrome, Firefox) that makes debugging and inspecting React applications much, much easier. With these tools installed, you can inspect both the state and performance of any component (or your app as a whole)."
+    },
+];
+
+export const angularInfo = [
+    {
+        id: 7,
+        title: "Observable Tutorial",
+        image: angular,
+        url: "https://www.tektutorialshub.com/angular/angular-observable-tutorial-using-rxjs/",
+        txt: "When we talk about Angular Observable, we hear a lot of terms like Reactive programming, data streams, Observable, Observers, RxJS, etc. It is very important to understand these terms before we start using the observables."
+    },
+    {
+        id: 8,
+        title: "Beginners Guide",
+        image: angular,
+        url: "https://www.tektutorialshub.com/angular-tutorial/",
+        txt: "We have created a simple and step by step tutorial for beginners to learn all the features of the Angular. The tutorial also covers some of the advanced Angular Tutorials."
+    },
+    {
+        id: 9,
+        title: "Top 10 Best Angular Tips by Community",
+        image: angular,
+        url: "https://indepth.dev/posts/1308/best-angular-tips",
+        txt: "Explore these awesome best practices for your Angular apps."
+    },
+];
+
+export const jsInfo = [
+    {
+        id: 10,
+        title: "React",
+        image: js,
+        url: "https://faztweb.com",
+        txt: "Lorem Ipsum React Amet"
+    },
+    {
+        id: 11,
+        title: "React2",
+        image: js,
+        url: "https://faztweb.com",
+        txt: "Lorem Ipsum React Amet"
+    },
+    {
+        id: 12,
+        title: "React3",
+        image: js,
+        url: "https://faztweb.com",
+        txt: "Lorem Ipsum React Amet"
+    },
+];
+
+export const javaInfo = [
+    {
+        id: 13,
+        title: "Java",
+        image: java,
+        url: "https://blog.faztweb.com",
+        txt: "Lorem Ipsum Java Amet"
+    },
+    {
+        id: 14,
+        title: "Java2",
+        image: java,
+        url: "https://blog.faztweb.com",
+        txt: "Lorem Ipsum Java Amet"
+    },
+    {
+        id: 15,
+        title: "Java3",
+        image: java,
+        url: "https://blog.faztweb.com",
+        txt: "Lorem Ipsum Java Amet"
+    },
+];
+
+export const cInfo = [
+    {
+        id: 16,
+        title: "Java",
+        image: c,
+        url: "https://blog.faztweb.com",
+        txt: "Lorem Ipsum Java Amet"
+    },
+    {
+        id: 17,
+        title: "Java2",
+        image: c,
+        url: "https://blog.faztweb.com",
+        txt: "Lorem Ipsum Java Amet"
+    },
+    {
+        id: 18,
+        title: "Java3",
+        image: c,
+        url: "https://blog.faztweb.com",
+        txt: "Lorem Ipsum Java Amet"
+    },
+];
+
