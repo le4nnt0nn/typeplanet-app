d46f3b10953d6807d79466ba9168be1def3ea724
Antonio <ditacj78@gmail.com>
2022-06-01 20:30:48 +0200

    deploy on heroku

--- server/index.js
+++ server/index.js
@@ -18,9 +18,9 @@ app.use('/api', routes);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    /*app.get('*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    }); */
+    }); 
 }
 
 module.exports = app;
