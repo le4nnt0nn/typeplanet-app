9bf37e37295bc963a3c64075650d8346c35fc923
Antonio <ditacj78@gmail.com>
2022-06-02 20:08:34 +0200

    fix on sendFile

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use('/api', routes);
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     }); 
 }
 
